5c614e3f70d742c08bbb00ab900089d1cd3df00e
Partiks <parthpatel2597@gmail.com>
2019-12-06 05:06:59 -0500

    V 3.1 Added third book.

--- app/src/main/java/edu/buffalo/cse622/plugins/FrameOperations.java
+++ app/src/main/java/edu/buffalo/cse622/plugins/FrameOperations.java
@@ -1,11 +1,8 @@
 package edu.buffalo.cse622.plugins;
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.text.Layout;
 import android.text.method.LinkMovementMethod;
@@ -19,7 +16,6 @@ import com.google.ar.core.AugmentedImage;
 import com.google.ar.core.AugmentedImageDatabase;
 import com.google.ar.core.Config;
 import com.google.ar.core.Frame;
-import com.google.ar.core.HitResult;
 import com.google.ar.core.Session;
 
 import com.google.ar.core.TrackingState;
@@ -37,7 +33,7 @@ import java.util.HashSet;
 
 public class FrameOperations {
 
-    private static final String TAG = "BookReviewPlugin:" + FrameOperations.class.getSimpleName();
+    private static final String PTAG = "PartiksTag";
     ArFragment arFragment;
     Resources dynamicResources;
     Context context;
@@ -46,16 +42,19 @@ public class FrameOperations {
     Boolean shouldAddModel=true;
     private ViewRenderable textRenderable;
     private ViewRenderable textRenderable2;
+    private ViewRenderable textRenderable3;
+
     AnchorNode bookAnchor;
     Node bookInfoNode;
     AnchorNode sreBookAnchor;
     Node sreBookInfoNode;
+    AnchorNode thrillerBookAnchor;
+    Node thrillerBookInfoNode;
 
 
     public FrameOperations(Resources dynamicResources2, ArFragment arFragment2, HashSet<AnchorNode> pluginObjects){
         this.arFragment = arFragment2;
         context=arFragment.getContext();
-        //context = arFragment.getContext();
         dynamicResources = dynamicResources2;
         this.pluginObjects = pluginObjects;
 
@@ -66,6 +65,10 @@ public class FrameOperations {
         layoutId = dynamicResources.getIdentifier("text_view2", "layout", "edu.buffalo.cse622.plugins");
         XmlResourceParser textViewXml2 = dynamicResources.getLayout(layoutId);
         View view2 = LayoutInflater.from(context).inflate(textViewXml2, null);
+
+        layoutId = dynamicResources.getIdentifier("text_view3", "layout", "edu.buffalo.cse622.plugins");
+        XmlResourceParser textViewXml3 = dynamicResources.getLayout(layoutId);
+        View view3 = LayoutInflater.from(context).inflate(textViewXml3, null);
 
         ViewRenderable.builder()
                 .setView(context, view)
@@ -85,23 +88,39 @@ public class FrameOperations {
                         }
                 );
 
-        Log.e("TEST Loading", "Constructor called");
+        ViewRenderable.builder()
+                .setView(context, view3)
+                .build()
+                .thenAccept(
+                        (renderable) -> {
+                            textRenderable3 = renderable;
+                        }
+                );
 
         Session session = arFragment.getArSceneView().getSession();
-        Config config = session.getConfig();
+
+        Config config = new Config(session);
+        config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+        config.setFocusMode(Config.FocusMode.AUTO);
+        session.configure(config);
+        arFragment.getArSceneView().setupSession(session);
+
         setupAugmentedImagesDb(config, session);
+        arFragment.getPlaneDiscoveryController().hide();
+        arFragment.getPlaneDiscoveryController().setInstructionView(null);
+        arFragment.getArSceneView().getPlaneRenderer().setEnabled(false);
+
     }
 
     private void processFrame(Frame frame) {
-        //Log.e(PTAG, "///////////////////////////////////// PROCESS FRAME IN PLUGIN CALLED");
 
         frame = arFragment.getArSceneView().getArFrame();
         Collection<AugmentedImage> augmentedImages = frame.getUpdatedTrackables(AugmentedImage.class);
-        // who will cry when you die book height: 18cm and width: 12 cm
-        // sre book width: ~19 cm and height = ~23 cms
+        // who will cry when you die book height: 17cm and width: 12 cm
+        // sre book width: ~18 cm and height = ~23 cms
+        // thriller book: width = 14 cm and height = 20.5 cms
         for (AugmentedImage augmentedImage : augmentedImages) {
             switch (augmentedImage.getTrackingState()){
-
                 case TRACKING:
                     if(augmentedImage.getTrackingMethod() == AugmentedImage.TrackingMethod.FULL_TRACKING){
 
@@ -116,7 +135,6 @@ public class FrameOperations {
                                 float scaledWidth = imageWidth/augmentedImage.getExtentX();
                                 float scaledHeight = imageHeight/augmentedImage.getExtentZ();
 
-                                Toast.makeText(context, "Detected BOOK!!", Toast.LENGTH_LONG).show();
                                 bookAnchor = new AnchorNode(augmentedImage.createAnchor(augmentedImage.getCenterPose()));
                                 bookInfoNode = new Node();
                                 bookInfoNode.setParent(bookAnchor);
@@ -147,7 +165,7 @@ public class FrameOperations {
                                 }
                             }
                         }else if(augmentedImage.getName().equals("sre-book")){
-                            //Log.e(PTAG, "SRE BOOK DETECTED <<<<<<<<<>>>>>>>>>>><<<<<<<<<<>>>>>>>>>>>>>");
+
                             // enable sre book card node
                             if(sreBookInfoNode == null){
                                 //only add if book was not recognized before
@@ -167,7 +185,6 @@ public class FrameOperations {
                                 sreBookInfoNode.setLocalScale(new Vector3(scaledWidth/4, scaledHeight/4, scaledWidth/4));
                                 sreBookInfoNode.setLocalPosition(new Vector3(0.01f*augmentedImage.getCenterPose().qx(), 0.25f* augmentedImage.getCenterPose().qy(), 1.8f*augmentedImage.getCenterPose().qz()));
                                 sreBookInfoNode.setLocalRotation(Quaternion.axisAngle(new Vector3(-1f, 0, 0), 90f));
-                                Toast.makeText(context, "Configured Book Node", Toast.LENGTH_LONG);
 
                                 TextView tv = (TextView) textRenderable2.getView();
 
@@ -188,26 +205,65 @@ public class FrameOperations {
                                 }
                             }
                         }
-                        else if(augmentedImage.getName().equals("earth")){
+                        else if(augmentedImage.getName().equals("those-we-left-behind")){
+                            if(thrillerBookInfoNode == null){
+                                //only add if book was not recognized before
+                                Log.e(PTAG, "THRILLER BOOK ADDING NODE<<<<<<<<<<>>>>>>>>>");
+                                float imageWidth = 0.14f;
+                                float imageHeight = 0.205f;
 
+                                float scaledWidth = imageWidth/augmentedImage.getExtentX();
+                                float scaledHeight = imageHeight/augmentedImage.getExtentZ();
+
+                                thrillerBookAnchor = new AnchorNode(augmentedImage.createAnchor(augmentedImage.getCenterPose()));
+                                thrillerBookInfoNode = new Node();
+                                thrillerBookInfoNode.setParent(thrillerBookAnchor);
+                                textRenderable3.setShadowCaster(false);
+                                textRenderable3.setShadowReceiver(false);
+                                thrillerBookInfoNode.setRenderable(textRenderable3);
+                                thrillerBookInfoNode.setLocalScale(new Vector3(scaledWidth/6, scaledHeight/10, scaledWidth/6));
+                                thrillerBookInfoNode.setLocalPosition(new Vector3(0.01f*augmentedImage.getCenterPose().qx(), 0.01f* augmentedImage.getCenterPose().qy(), 0.01f*augmentedImage.getCenterPose().qz()));
+                                thrillerBookInfoNode.setLocalRotation(Quaternion.axisAngle(new Vector3(-1f, 1f, 1f), 90f));
+
+                                TextView tv = (TextView) textRenderable3.getView();
+
+                                //getting the text content and URL from the strings.xml values file
+                                int stringId = dynamicResources.getIdentifier("book_those_we_left_behind", "string", "edu.buffalo.cse622.plugins");
+                                tv.setText(dynamicResources.getText(stringId));
+                                tv.setMovementMethod(LinkMovementMethod.getInstance());
+                                tv.setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);
+
+                                //getting the rounded background textbox from green_rounded_bg.xml layout file
+                                int bgId = dynamicResources.getIdentifier("red_rounded_bg", "drawable", "edu.buffalo.cse622.plugins");
+                                Drawable background;
+                                try {
+                                    background = Drawable.createFromXml(dynamicResources, dynamicResources.getXml(bgId));
+                                    tv.setBackground(background);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
                         }
 
                     }else{
                         // remove this object's anchor node
-                        if(augmentedImage.getName().equals("who-will-cry-when-you-die")){
-                            arFragment.getArSceneView().getScene().removeChild(bookInfoNode);
+                        if(augmentedImage.getName().equals("who-will-cry-when-you-die") && bookAnchor != null){
+                            //arFragment.getArSceneView().getScene().removeChild(bookInfoNode);
                             bookInfoNode=null;
                             arFragment.getArSceneView().getScene().removeChild(bookAnchor);
                             bookAnchor=null;
                         }
-                        if(augmentedImage.getName().equals("sre-book")){
-                            arFragment.getArSceneView().getScene().removeChild(sreBookInfoNode);
+                        if(augmentedImage.getName().equals("sre-book") && sreBookAnchor != null){
+                            //arFragment.getArSceneView().getScene().removeChild(sreBookInfoNode);
                             arFragment.getArSceneView().getScene().removeChild(sreBookAnchor);
                             sreBookInfoNode=null;
                             sreBookAnchor=null;
                         }
-                        if(augmentedImage.getName().equals("earth")){
-
+                        if(augmentedImage.getName().equals("those-we-left-behind") && thrillerBookAnchor != null){
+                            //arFragment.getArSceneView().getScene().removeChild(thrillerBookInfoNode);
+                            arFragment.getArSceneView().getScene().removeChild(thrillerBookAnchor);
+                            thrillerBookInfoNode=null;
+                            thrillerBookAnchor=null;
                         }
                     }
             }
@@ -219,44 +275,16 @@ public class FrameOperations {
             pluginObjects.add(bookAnchor);
         }
         if (sreBookAnchor != null) {
-            Log.e(TAG, "ADDED SRE BOOK TO THE SCENE");
-            Toast.makeText(context, "ADDED SRE Book To Scene", Toast.LENGTH_LONG);
             sreBookAnchor.setParent(arFragment.getArSceneView().getScene());
             pluginObjects.add(sreBookAnchor);
         }
-    }
-
-    private void planeTap(HitResult hitResult) {
-    }
-
-    private void onDestroy() {
+        if(thrillerBookAnchor!=null){
+            thrillerBookAnchor.setParent(arFragment.getArSceneView().getScene());
+            pluginObjects.add(thrillerBookAnchor);
+        }
     }
 
     public boolean setupAugmentedImagesDb(Config config, Session session) {
-        AugmentedImageDatabase augmentedImageDatabase = config.getAugmentedImageDatabase();
-
-        Bitmap bitmap1 = loadAugmentedImage("who-will-cry-when-you-die.jpg");
-        if (bitmap1 == null) {
-            return false;
-        }
-
-        augmentedImageDatabase.addImage("who-will-cry-when-you-die", bitmap1);
-        config.setAugmentedImageDatabase(augmentedImageDatabase);
-
-        Bitmap bitmap2 = loadAugmentedImage("sre-book.jpg");
-        if (bitmap2 == null) {
-            return false;
-        }
-
-        augmentedImageDatabase.addImage("sre-book", bitmap2);
-        config.setAugmentedImageDatabase(augmentedImageDatabase);
-
-        session.configure(config);
-
-        return true;
-
-        /*
-        // CORE LOGIC START ----------------------->
         AugmentedImageDatabase augmentedImageDatabase;
         try {
             //new way of loading things without the integer ID
@@ -269,92 +297,12 @@ public class FrameOperations {
         }
 
         config.setAugmentedImageDatabase(augmentedImageDatabase);
-        // CORE LOGIC OVER ----------------------->
         //printing number of Augmented Images inside the DB to verify if everything is working fine.
         Log.e(PTAG, "INSIDE APK SESSION DB CONTENTS = " + augmentedImageDatabase.getNumImages());
-        //Toast.makeText(context, "INSIDE APK SESSION DB CONTENTS = " + augmentedImageDatabase.getNumImages(), Toast.LENGTH_SHORT).show();
 
         session.configure(config);
 
         return true;
-        */
     }
 
-    private Bitmap loadAugmentedImage(String fileName) {
-        try {
-            AssetManager assetManager = dynamicResources.getAssets();
-            InputStream is = assetManager.open(fileName);
-            return BitmapFactory.decodeStream(is);
-        } catch (IOException e) {
-            Log.e(TAG, "loadAugmentedImage: " + "IO Exception", e);
-        }
-        return null;
-    }
-// -------------- BOX OPERATIONS BASIC TEST PLUGINS -----------------
-// BELOW CODE NOT USED IN BOOK REVIEW PLUGIN
-    /*public FrameOperations( Resources dynamicResources2, ArFragment arFragment2){
-
-        context=arFragment2.getContext();
-        //context = arFragment.getContext();
-        dynamicResources = dynamicResources2;
-
-        this.arFragment = arFragment2;
-        // This is how we load a layout resource.
-        int layoutId = dynamicResources.getIdentifier("text_view", "layout", "edu.buffalo.cse622.plugins");
-        XmlResourceParser textViewXml = dynamicResources.getLayout(layoutId);
-        View view = LayoutInflater.from(context).inflate(textViewXml, null);
-
-
-        ViewRenderable.builder()
-                .setView(context, view)
-                .build()
-                .thenAccept(
-                        (renderable) -> {
-                            textRenderable = renderable;
-                        }
-                );
-        node = new Node();
-
-        Log.e("TEST Loading", "Constructor called");
-        Toast.makeText(context, "New APK!", Toast.LENGTH_LONG).show();
-    }
-
-    private Node processFrame(Frame frame) {
-        for (Plane plane : frame.getUpdatedTrackables(Plane.class)) {
-            if(plane.getTrackingState() == TrackingState.TRACKING ) {
-                Anchor anchor = plane.createAnchor(plane.getCenterPose());
-                AnchorNode anchorNode = new AnchorNode(anchor);
-                node = new Node();
-                node.setParent(anchorNode);
-                node.setRenderable(textRenderable);
-                node.setLocalPosition(new Vector3(-0.5f*plane.getCenterPose().qx()+0.15f, -2f*plane.getCenterPose().qy(), -2f * plane.getCenterPose().qz() ));
-                node.setLocalRotation(Quaternion.axisAngle(new Vector3(-1f, 0, 0), 90f));
-
-                TextView tv = (TextView) textRenderable.getView();
-
-                //tv.setBackgroundColor(Color.parseColor("#0000FF"));
-                //#228B22
-                int stringId = dynamicResources.getIdentifier("partiks_hello", "string", "edu.buffalo.cse622.plugins");
-                tv.setText(dynamicResources.getString(stringId));
-                int bgId = dynamicResources.getIdentifier("green_rounded_bg", "drawable", "edu.buffalo.cse622.plugins");
-                Drawable background;
-                try {
-                    background = Drawable.createFromXml(dynamicResources, dynamicResources.getXml(bgId));
-                    tv.setBackground(background);
-                    tv.setBackgroundColor(Color.parseColor("#228B22"));
-                }catch(Exception e){
-                    e.printStackTrace();
-                }
-                //tv.setBackgroundResource(bgId);
-
-
-                //-tv.setText(context.getString(R.string.partiks_hello));
-
-
-                break;
-            }
-        }
-
-        return node;
-    } */
 }
